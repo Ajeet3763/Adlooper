82929857f07140586cc5da95d47359374e6d6993
Ajeet3763 <negiajeet3763@gmail.com>
2024-06-02 10:33:34 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+// -------------------Mobile Nav Bar-------------------
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+const header = document.querySelector(".page-header");
+const toggleClass = "is-sticky";
+    
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > 150) {
+        header.classList.add(toggleClass);
+    } else {
+        header.classList.remove(toggleClass);
+    }
+});
